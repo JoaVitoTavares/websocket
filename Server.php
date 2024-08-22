7102e858fbc671daa7b2810b19c6944199c32f3d
João Vitor <joaovitavares@outlook.com>
2024-08-22 10:15:14 -0300

    Ajusta mensagens de log

---
+++ Server.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WebsocketTest;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new WebSocket()
+        )
+    ),
+    8080
+);
+
+$data_str = var_export("chega até aqui \n", true);
+
+// Write the data into the file
+$file_path = __DIR__ . '/dump.txt';
+file_put_contents($file_path, $data_str . "\n", FILE_APPEND);
+
+$server->run();
+
+
---
+++ WebSocket.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace WebsocketTest;
+
+use Ratchet\ConnectionInterface;
+use Ratchet\MessageComponentInterface;
+
+class WebSocket implements MessageComponentInterface
+{
+
+    public function  __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+    }
+
+    function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+    }
+
+    function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        // Get a string representation of the data
+        $data_str = var_export($e, true);
+
+        // Write the data into the file
+        $file_path = __DIR__ . '/dump.txt';
+        file_put_contents($file_path, $data_str);
+
+        error_log($e->getMessage(). "\n \n", 3, __DIR__ . "/error.txt");
+
+        $conn->close();
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach ($this->clients as $client) {
+            if ($from != $client) {
+                $client->send($msg);
+            }
+        }
+    }
+}
